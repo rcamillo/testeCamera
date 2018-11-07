9c8d2bf16507a331b044805731500b2b8cc9a665
Renan Camillo - Note <rcamillo12@gmail.com>
2018-11-07 20:52:23 -0200

    rolezinho da camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,48 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  public imagem = 'teste';
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public camera: Camera) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  abreOSatanas() {
+    //options do role
+    const options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 500,
+      targetHeight: 500,
+      allowEdit: true
+      //encodingType: this.camera.EncodingType.JPEG,
+      //mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.imagem = base64Image;
+      console.log(this.imagem);
+    }, (err) => {
+      // Handle error 
+    });
+
+    console.log('demonio');
   }
 }
 
