6daa72dbe4ad54c778b45eeabf8f22b36e668654
Renan Camillo - Note <rcamillo12@gmail.com>
2018-11-08 01:39:44 -0200

    Apenas Copia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { Camera, CameraOptions } from "@ionic-native/camera";
 
-import { HomePage } from '../pages/home/home';
+import { HomePage } from "../pages/home/home";
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
   rootPage: any = HomePage;
 
-  public imagem = 'teste';
+  public imagem = "teste";
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public camera: Camera) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public camera: Camera
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -31,19 +36,21 @@ export class MyApp {
       allowEdit: true
       //encodingType: this.camera.EncodingType.JPEG,
       //mediaType: this.camera.MediaType.PICTURE
-    }
+    };
 
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64 (DATA_URL):
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-      this.imagem = base64Image;
-      console.log(this.imagem);
-    }, (err) => {
-      // Handle error 
-    });
+    this.camera.getPicture(options).then(
+      imageData => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        let base64Image = "data:image/jpeg;base64," + imageData;
+        this.imagem = base64Image;
+        console.log(this.imagem);
+      },
+      err => {
+        // Handle error
+      }
+    );
 
-    console.log('demonio');
+    console.log("demonio");
   }
 }
-
